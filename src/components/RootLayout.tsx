25fe13c7befade35039ce10272b158e76997e962
Arsh Verma <arshverma.dev@gmail.com>
2026-01-24 01:47:53 +0530

    Update RootLayout.tsx

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,23 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Breadcrumbs from './Breadcrumbs';
 import Footer from './Footer';
-import MobileNav from './MobileNav';
 
 export default function RootLayout() {
+    const location = useLocation();
+    const isEditor = location.pathname === '/editor';
 
     return (
         <div className="min-h-screen flex flex-col bg-white overflow-hidden">
-            <Navbar />
-            <div className="pt-24 pb-20 md:pb-0 flex-1">
-                <Breadcrumbs />
-                <main className="flex-1">
+            {!isEditor && <Navbar />}
+            <div className={`${isEditor ? 'h-screen' : 'pt-24 pb-20 md:pb-0'} flex-1 flex flex-col`}>
+                {!isEditor && <Breadcrumbs />}
+                <main className="flex-1 relative">
                     <Outlet />
                 </main>
             </div>
-            <Footer />
-            <MobileNav />
-            <MobileNav />
+            {!isEditor && <Footer />}
         </div>
     );
 }
