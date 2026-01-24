55620c1726f830cff45e032d55f33f75c0273b98
Arsh Verma <arshverma.dev@gmail.com>
2026-01-24 19:24:30 +0530

    Update RootLayout.tsx

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
     return (
         <div className="min-h-screen flex flex-col bg-white">
             <Navbar />
-            <div className="pt-24 flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col">
                 <main className="flex-1 relative">
                     <Outlet />
                 </main>
