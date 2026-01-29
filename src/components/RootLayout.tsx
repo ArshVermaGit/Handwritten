77b54154fdac92569857490f36448569ae039030
Arsh Verma <arshverma.dev@gmail.com>
2026-01-29 18:54:31 +0530

    Update RootLayout.tsx

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -9,15 +9,15 @@ export default function RootLayout() {
     return (
         <div className="min-h-screen flex flex-col bg-white">
             <Navbar />
-            <div className="flex-1 flex flex-col pt-16 sm:pt-20">
+            <div className="flex-1 flex flex-col">
                 <main className="flex-1 relative">
                     <AnimatePresence mode="wait">
                         <motion.div
                             key={location.pathname}
-                            initial={{ opacity: 0, y: 10 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0, y: -10 }}
-                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.3, ease: "easeOut" }}
                         >
                             <Outlet />
                         </motion.div>
